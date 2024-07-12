3be58f6f112ecc8fa6329634059b7be0f028f67c
gp-kobayashi <kobayashi_y@gp1000.jp>
2024-07-12 17:08:35 +0900

    [new]todoの配列の追加まで

--- pages/index.js
+++ pages/index.js
@@ -1,65 +1,49 @@
 import Head from 'next/head'
 import styles from '../styles/Home.module.css'
+import { useCallback, useState } from 'react'
+
 
 export default function Home() {
+
+  const [text,setText] = useState("");
+  const [array,setArray] = useState([]);
+  
+  const handleChange =(e) =>{
+    setText(e.target.value.trim());
+  }
+  
+  const handleAdd = useCallback(() =>{
+    setArray((prevArray) =>{
+      const newArray = [...prevArray,text]
+      return newArray;
+    });
+  },[text]);
+
   return (
     <div className={styles.container}>
       <Head>
-        <title>Create Next App</title>
+        <title>todo-study</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
       <main className={styles.main}>
-        <h1 className={styles.title}>
-          Welcome to <a href="https://nextjs.org">Next.js!</a>
-        </h1>
-
-        <p className={styles.description}>
-          Get started by editing{' '}
-          <code className={styles.code}>pages/index.js</code>
-        </p>
-
-        <div className={styles.grid}>
-          <a href="https://nextjs.org/docs" className={styles.card}>
-            <h3>Documentation &rarr;</h3>
-            <p>Find in-depth information about Next.js features and API.</p>
-          </a>
-
-          <a href="https://nextjs.org/learn" className={styles.card}>
-            <h3>Learn &rarr;</h3>
-            <p>Learn about Next.js in an interactive course with quizzes!</p>
-          </a>
-
-          <a
-            href="https://github.com/vercel/next.js/tree/master/examples"
-            className={styles.card}
-          >
-            <h3>Examples &rarr;</h3>
-            <p>Discover and deploy boilerplate example Next.js projects.</p>
-          </a>
-
-          <a
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-            className={styles.card}
-          >
-            <h3>Deploy &rarr;</h3>
-            <p>
-              Instantly deploy your Next.js site to a public URL with Vercel.
-            </p>
-          </a>
+        <h1>TODOリスト</h1>
+        <div className={styles.todo}>
+          <form>
+            <input type='text' value={text} onChange={handleChange}/>
+          </form>
+          <button onClick={handleAdd}>追加</button>
+          <ul>
+            {array.map(item => {
+              return <div>
+                <li key ={item}>{item}</li>
+                <button>削除</button>
+                </div>
+            })}
+          </ul>
+          
         </div>
       </main>
-
-      <footer className={styles.footer}>
-        <a
-          href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Powered by{' '}
-          <img src="/vercel.svg" alt="Vercel Logo" className={styles.logo} />
-        </a>
-      </footer>
     </div>
   )
 }
